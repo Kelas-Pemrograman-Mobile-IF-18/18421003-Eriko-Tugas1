c15225637c015b2d90aaf78e4e6ea40eb09c3851
Eriko <erikomedia123@gmail.com>
2021-03-25 15:51:24 +0700

    update

--- app/src/main/java/com/eriko/prj1/MainActivity.java
+++ app/src/main/java/com/eriko/prj1/MainActivity.java
@@ -12,9 +12,8 @@ import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtTampil, txtJudul;
-    EditText edtNpm, edtNama, edtAlamat, edtUmur, edtTelepon;
-    Button btnSubmit;
+    TextView txtNama, txtNpm, txtUmur, txtAlamat,  txtJudul;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +22,17 @@ public class MainActivity extends AppCompatActivity {
 
         txtJudul = (TextView) findViewById(R.id.txtJudul);
         txtJudul.setText("Biodata");
-        txtTampil = (TextView) findViewById(R.id.txtBiodata);
 
-        edtNpm = (EditText) findViewById(R.id.edtNpm);
-        edtNama = (EditText) findViewById(R.id.edtNama);
-        edtUmur = (EditText) findViewById(R.id.edtUmur);
-        edtAlamat = (EditText) findViewById(R.id.edtAlamat);
-        edtTelepon = (EditText) findViewById(R.id.edtTelepon);
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        txtNama = (TextView) findViewById(R.id.txtNama);
+        txtNama.setText("Eriko Zakaria");
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String strNpm = edtNpm.getText().toString();
-                String strNama = edtNama.getText().toString();
-                String strUmur = edtUmur.getText().toString();
-                String strAlamat = edtAlamat.getText().toString();
-                String strTelepon = edtTelepon.getText().toString();
-                txtTampil.setText(strNpm + "\n" + strNama + "\n" + strUmur+ "\n" + strAlamat + "\n" + strTelepon);
-            }
-        });
+        txtNpm = (TextView) findViewById(R.id.txtNpm);
+        txtNpm.setText("18421003");
+
+        txtUmur = (TextView) findViewById(R.id.txtUmur);
+        txtUmur.setText("21");
+
+        txtAlamat = (TextView) findViewById(R.id.txtAlamat);
+        txtAlamat.setText("Jl. Kartini gg. Setia Negara No. 46");
     }
 }
